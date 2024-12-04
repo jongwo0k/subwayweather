ef27905b77a02f19ad899bd2e554d722108c36fe
jongwo0k <95351704+jongwo0k@users.noreply.github.com>
2024-12-05 02:20:08 +0900

    Add files via upload

---
+++ subwayweather/controllers/realTimeController.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const axios = require('axios');
+
+const getRealTimeSubwayInfo = async (stationName) => {
+    try {
+        const subwayApiKey = process.env.SUBWAY_API_KEY;
+        const url = `http://swopenAPI.seoul.go.kr/api/subway/${subwayApiKey}/json/realtimeStationArrival/0/5/${encodeURIComponent(
+            stationName
+        )}`;
+        console.log('API 요청 URL:', url);
+
+        const response = await axios.get(url);
+
+        const data = response.data.realtimeArrivalList;
+        if (!data || data.length === 0) {
+            throw new Error(`실시간 도착 정보를 가져올 수 없습니다: ${stationName}`);
+        }
+
+        const subwayInfo = data.map((item) => ({
+            direction: item.trainLineNm,
+            arrivalTimeSeconds: item.barvlDt,
+            firstArrivalMessage: item.arvlMsg2,
+        }));
+
+        return subwayInfo;
+    } catch (error) {
+        console.error('실시간 도착 정보를 가져오는데에 error 발생:', error.message);
+        throw error;
+    }
+};
+
+module.exports = { getRealTimeSubwayInfo };
