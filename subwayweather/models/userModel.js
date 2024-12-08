fcf54e886a4a62c895598bb5d7817601d0c8af0e
jongwo0k <95351704+jongwo0k@users.noreply.github.com>
2024-12-09 00:42:28 +0900

    Update userModel.js

--- subwayweather/models/userModel.js
+++ subwayweather/models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  email: { type: String, required: false },
+  email: { type: String, match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, required: false },
 });
 
 module.exports = mongoose.model('User', userSchema);
