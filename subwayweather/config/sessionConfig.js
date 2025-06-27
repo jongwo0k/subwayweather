11631bf1b02837ccd71b5d82d5fd05920b46ef81
jongwo0k <95351704+jongwo0k@users.noreply.github.com>
2025-06-27 11:00:36 +0900

    session management

--- subwayweather/config/sessionConfig.js
+++ subwayweather/config/sessionConfig.js
@@ -7,6 +7,7 @@ const sessionConfig = {
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
+  rolling: true,
   store: MongoStore.create({
     mongoUrl: process.env.MONGO_URI,
     collectionName: 'sessions',
@@ -14,7 +15,7 @@ const sessionConfig = {
   // 쿠키
   cookie: {
     httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24, // 1일
+    maxAge: 1000 * 60 * 60 * 2, // 2시간
   },
 };
 
